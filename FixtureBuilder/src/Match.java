8ac40ce7fa87ac11adb9a97721fb4add52829596
mustafaguler3 <mustafa.glr@hotmail.com>
2022-02-14 21:25:30 +0300

    fixture builder

---
+++ FixtureBuilder/src/Match.java
@@ -0,0 +1,26 @@
+public class Match {
+
+    private String homeTeam;
+    private String awayTeam;
+
+    public Match(String homeTeam, String awayTeam) {
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+    }
+
+    public String getHomeTeam() {
+        return homeTeam;
+    }
+
+    public void setHomeTeam(String homeTeam) {
+        this.homeTeam = homeTeam;
+    }
+
+    public String getAwayTeam() {
+        return awayTeam;
+    }
+
+    public void setAwayTeam(String awayTeam) {
+        this.awayTeam = awayTeam;
+    }
+}
